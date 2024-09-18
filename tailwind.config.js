eabb09f3400485faea7ea76b12ab11d1628b4c3f
antonfiluta <antonfiluta2.0@mail.ru>
2024-09-18 17:52:45 +0300

    feat: starting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["*.html"],
   theme: {
+    colors: {
+      'dr': '#380F17',
+      'mr': '#8F0B13',
+      'lr': '#DC2011',
+      'l': '#EFDFC5',
+      'g': '#252B2B',
+      'white': '#FFFFFF',
+      'black': '#000000',
+    },
+    // fontFamily: {
+    //   OpenGost: ['OpenGost Type A TT', 'OpenGost Type A TT'],
+    //   sans: ['Roboto', 'Roboto'],
+    //   bebas: ['Bebas', 'Bebas Neue'],
+    //   znikomit: ['ZnikomitNo25', 'ZnikomitNo25'],
+    // },
     extend: {},
   },
   plugins: [],
